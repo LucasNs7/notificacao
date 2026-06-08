fc235ef0f4580be72dae7ab3c9922237cbb11d37
Lucas Sousa <lucas.sousadev07@gmail.com>
2026-06-08 10:12:23 -0300

    initial commit

---
+++ src/main/java/com/lucas/notificacoes/NotificacoesApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.notificacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificacoesApplication.class, args);
+	}
+
+}
